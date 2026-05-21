d0a4d36a8e63ce61dc91fae13177a6d150eca584
anthopyta <idhamadileksono@gmail.com>
2026-05-22 01:26:49 +0700

    Add authentication features and update dependencies
    Implemented user authentication features including login, registration, password reset, and email verification. Created necessary controllers, requests, and views for handling user authentication flows. Updated package dependencies to include Laravel Breeze for authentication scaffolding, and added Tailwind CSS and Alpine.js for styling and interactivity. Configured PostCSS and Tailwind settings for the project.

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,4 +35,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Transaction::class);
     }
+
+    public function isAdmin(): bool
+    {
+        return $this->role === 'admin';
+    }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,6 +26,7 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'role' => 'customer',
         ];
@@ -38,6 +39,16 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'role' => 'admin',
+        ]);
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
         ]);
     }
 }
